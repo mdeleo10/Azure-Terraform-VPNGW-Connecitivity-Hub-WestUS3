4d71bc5f41bf3bb2b89cadf6ec8c063b31b1eb98
Michael De Leo <mdeleo10@hotmail.com>
2023-08-11 11:50:48 -0400

    Push

--- vpngw.tf
+++ vpngw.tf
@@ -49,7 +49,7 @@ terraform {
     resource_group_name = azurerm_resource_group.rg.name
     allocation_method = "Dynamic"
     # Standard SKU for Internal IPSec tunnel
-#    sku               = "Standard"
+    sku               = "Standard"
   }
   
 # Create VPN GW
@@ -61,11 +61,11 @@ terraform {
     type     = "Vpn"
     vpn_type = "RouteBased"
   
-    active_active = false
+    active_active = true
     enable_bgp    = true
   # sku = "standard" for non-internal IPSec tunnel
- #   sku           = "VpnGw2AZ"
-    sku            = "Standard"
+    sku           = "VpnGw2AZ"
+ #   sku            = "Standard"
 
     ip_configuration {
       name                          = "vnetGatewayConfig"
@@ -74,48 +74,9 @@ terraform {
       subnet_id                     = azurerm_subnet.vpn_gw.id
     }
 
-  # Adding support for Internal VPN - for example IPSec tunnel over Express Route
-#    private_ip_address_enabled    = true
+# Adding support for Internal VPN - for example IPSec tunnel over Express Route
+    private_ip_address_enabled    = true
   
-  # Define VPN Point-to-Site including address space and public cert key
-    vpn_client_configuration {
-      address_space = ["${var.vpn_gw_vpn_client_configuration_address_space}"]
-  
-      root_certificate {
-        name = "DigiCert-Federated-ID-Root-CA"
-  
-        public_cert_data = <<EOF
-  MIIDuzCCAqOgAwIBAgIQCHTZWCM+IlfFIRXIvyKSrjANBgkqhkiG9w0BAQsFADBn
-  MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
-  d3cuZGlnaWNlcnQuY29tMSYwJAYDVQQDEx1EaWdpQ2VydCBGZWRlcmF0ZWQgSUQg
-  Um9vdCBDQTAeFw0xMzAxMTUxMjAwMDBaFw0zMzAxMTUxMjAwMDBaMGcxCzAJBgNV
-  BAYTAlVTMRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdp
-  Y2VydC5jb20xJjAkBgNVBAMTHURpZ2lDZXJ0IEZlZGVyYXRlZCBJRCBSb290IENB
-  MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvAEB4pcCqnNNOWE6Ur5j
-  QPUH+1y1F9KdHTRSza6k5iDlXq1kGS1qAkuKtw9JsiNRrjltmFnzMZRBbX8Tlfl8
-  zAhBmb6dDduDGED01kBsTkgywYPxXVTKec0WxYEEF0oMn4wSYNl0lt2eJAKHXjNf
-  GTwiibdP8CUR2ghSM2sUTI8Nt1Omfc4SMHhGhYD64uJMbX98THQ/4LMGuYegou+d
-  GTiahfHtjn7AboSEknwAMJHCh5RlYZZ6B1O4QbKJ+34Q0eKgnI3X6Vc9u0zf6DH8
-  Dk+4zQDYRRTqTnVO3VT8jzqDlCRuNtq6YvryOWN74/dq8LQhUnXHvFyrsdMaE1X2
-  DwIDAQABo2MwYTAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNV
-  HQ4EFgQUGRdkFnbGt1EWjKwbUne+5OaZvRYwHwYDVR0jBBgwFoAUGRdkFnbGt1EW
-  jKwbUne+5OaZvRYwDQYJKoZIhvcNAQELBQADggEBAHcqsHkrjpESqfuVTRiptJfP
-  9JbdtWqRTmOf6uJi2c8YVqI6XlKXsD8C1dUUaaHKLUJzvKiazibVuBwMIT84AyqR
-  QELn3e0BtgEymEygMU569b01ZPxoFSnNXc7qDZBDef8WfqAV/sxkTi8L9BkmFYfL
-  uGLOhRJOFprPdoDIUBB+tmCl3oDcBy3vnUeOEioz8zAkprcb3GHwHAK+vHmmfgcn
-  WsfMLH4JCLa/tRYL+Rw/N3ybCkDp00s0WUZ+AoDywSl0Q/ZEnNY0MsFiw6LyIdbq
-  M/s/1JRtO3bDSzD9TazRVzn2oBqzSa8VgIo5C1nOnoAKJTlsClJKvIhnRlaLQqk=
-  EOF
-  
-      }
-  
-      revoked_certificate {
-        name       = "Verizon-Global-Root-CA"
-        thumbprint = "912198EEF23DCAC40939312FEE97DD560BAE49B1"
-      }
-    }
-  }
-
 # Create and define Local Network Gateway for Site-to-Site connection with BGP to external IP address
 resource "azurerm_local_network_gateway" "remote_net" {
     name                = "local-network-gateway-remote-${azurerm_resource_group.rg.location}"
