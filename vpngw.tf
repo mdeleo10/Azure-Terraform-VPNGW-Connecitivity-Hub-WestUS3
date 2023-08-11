fdbffa95c6c2f28f78da6e80c941a9d3ca2302ad
Michael De Leo <mdeleo10@hotmail.com>
2023-08-11 16:45:40 -0400

    Initial Testing

--- vpngw.tf
+++ vpngw.tf
@@ -52,7 +52,7 @@ terraform {
   
     # Standard SKU for Internal IPSec tunnel
     sku               = "Standard"
- #   zones             = ["1","2"]
+    zones             = ["1","2","3"]
   }
 
 # Create Public IP 2 for VPN GW
@@ -65,7 +65,7 @@ terraform {
   
     # Standard SKU for Internal IPSec tunnel
     sku               = "Standard"
-  #  zones             = ["1","2"]
+    zones             = ["1","2","3"]
   }
 
 # Create VPN GW
